6a89c55cc2ff4893df8f265cd6c6f4e63a4f6a9d
cly <cly970110@163.com>
2019-07-07 13:57:14 +0800

    第一次合并酒店

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-        酒店首页
+    <div class="container">
+        <!-- 地图模块 -->
+        <Map @setHotelList="setHotelList"/>
+        <!-- 酒店模块 -->
+        <HotelModel :hotelList="hotelList"/>
     </div>
 </template>
 
 <script>
+import Map from "@/components/hotel/map.vue"
+import HotelModel from "@/components/hotel/hotelModel.vue"
 export default {
-
+    data(){
+        return{
+            hotelList:[]
+        }
+    },
+    components:{
+        Map,
+        HotelModel
+    },
+    mounted(){
+        this.$router.push({
+        path:"/hotel?city=74",
+      })
+    },
+    methods:{
+        setHotelList(data){
+            this.hotelList=data;
+        }
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .container {
+        width: 1000px;
+        margin: 0 auto;
+    }
 </style>
